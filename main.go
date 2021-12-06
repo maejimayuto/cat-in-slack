23590f2c3a7766e63a01d12077fec578590264f7
maejimayuto <maejimayuto@gmail.com>
2021-12-06 22:39:45 +0900

    add: comment

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ func main() {
 	}
 	cats := cat.GetCats()
 	fmt.Printf("%s", cats[0].ToString())
+	// TODO: 2MB 以上だと画像が展開されないので、別の画像に変更する #1
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	// TODO: 画像の block だけをいい感じに表示したい #2
 	if _, _, err := api.PostMessage("04-cat", slack.MsgOptionText(cats[0].Url, false)); err != nil {
 		log.Println(err)
 		return
