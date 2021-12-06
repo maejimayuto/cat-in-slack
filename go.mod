89913a91ca1fee0f195034fee3a701eca50668ee
maejimayuto <maejimayuto@gmail.com>
2021-12-06 21:52:44 +0900

    add: sending message

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module cat-in-slack
 
 go 1.17
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"cat-in-slack/cat"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/slack-go/slack"
 )
 
 func main() {
 	result := cat.GetCat()
-	fmt.Printf("%s\n", result)
+	fmt.Printf("%s", result)
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	if _, _, err := api.PostMessage("04-cat", slack.MsgOptionText("test", false)); err != nil {
+		log.Println(err)
+		return
+	}
 }
